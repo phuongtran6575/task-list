2c9c9c5a51e124bc313a168383c0b950e59749a7
keoma <keomacnha690@gmail.com>
2025-08-04 23:07:51 +0700

    fix auth service

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -1,3 +1,11 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlmodel import select
 from models.user_models import User
 from passlib.context import CryptContext
+from todolist.app.database.sqlite_database_config import SessionDepends
+
+def get_user_by_username(session: SessionDepends, username: str):
+    statement = select(User).where(User.username == username)
+    user_data = session.exec(statement).first()
+    if user_data:
+        return 
